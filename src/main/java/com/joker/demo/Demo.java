4b6bb29677cb1c3463e6d8fe371d5d71768fe3cc
joker-mac <lpinzhou@163.com>
2020-01-15 01:20:12 +0800

    transfer to maven project

---
+++ src/main/java/com/joker/demo/Demo.java
@@ -0,0 +1,22 @@
+/**
+ * 
+ */
+package com.joker.demo;
+
+/**
+ * @author joker
+ * @ClassName Demo
+ * @
+ */
+public class Demo {
+
+	/**
+	 * Demo
+	 * @param args
+	 * void
+	 */
+	public static void main(String[] args) {
+		System.out.println("HelloWorld!");
+	}
+
+}
